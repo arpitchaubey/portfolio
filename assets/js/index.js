4da42be00b869389c5ce9c6166538f476fe0d67a
ArpitChaubey <127391287+arpitchaubey@users.noreply.github.com>
2025-01-29 11:49:20 +0530

    animation scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,3 +22,12 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+window.addEventListener('scroll', function () {
+    let navbar = document.querySelector('.custom-navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('sticky-nav');
+    } else {
+        navbar.classList.remove('sticky-nav');
+    }
+});
